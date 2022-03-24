490d907af55d4253ac236efe4d708b698af0ec56
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-24 14:40:21 +0100

    aggiunti commenti

--- PRJ-MAIL/main.js
+++ PRJ-MAIL/main.js
@@ -2,17 +2,20 @@ let contatti = ["matteo@gmail.com", "fabio@gmail.com", "pietro@gmail.com", "anna
 console.log(contatti);
 
 alert("Dobbiamo verificare se puoi accedere al nostro database")
+//dichiaro l'email da confrontare
 let email = prompt("Digita la tua email");
 console.log(email)
-let variabileConfronto
+let trasporto
 
 for(let i = 0; i<contatti.length; i++){
     if (email==contatti[i]){
-        alert("La tua email è nel database");
-        variabileConfronto = email;
+        alert("La tua email è nel database, benvenut*");
+        /* se l'email è presente assegnamo alla trasporto la stessa email per trasportarla fuori dal for */
+        trasporto = email;
     } 
 }
 
-if (variabileConfronto != email){
+/*confrontiamo la trasporto con email, se è diversa eseguiamo l'allert*/
+if (trasporto != email){
     alert("la tua email non è presente nel database");
 }
