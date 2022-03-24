31a9d2ba6e81b22f4f460c30fb2eb25461584bf8
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-24 16:30:35 +0100

    commit con esercizio bonus

--- PRJ-MAIL/bonus/bonus.js
+++ PRJ-MAIL/bonus/bonus.js
@@ -9,13 +9,21 @@ let trasporto
 
 for(let i = 0; i<contatti.length; i++){
     if (email==contatti[i]){
-        alert("La tua email è nel database, benvenuto");
+        alert("La tua email è nel database, benvenut*");
         /* se l'email è presente assegnamo alla trasporto la stessa email per trasportarla fuori dal for */
         trasporto = email;
     } 
 }
-
+let risposta;
 /*confrontiamo la trasporto con email, se è diversa eseguiamo l'allert*/
 if (trasporto != email){
     alert("la tua email non è presente nel database");
+    risposta = prompt("vuoi aggiungere la tua email?")
 }
+
+    if(risposta == "si") {
+        let mailDaAggiungere= prompt('inserisci la nuova email');
+        contatti.push(mailDaAggiungere);
+        alert("hai aggiunto la tua email correttamente");
+        console.log(contatti);
+    } 
