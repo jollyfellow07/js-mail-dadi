b67dd6bf7a00738d1443e881670147e4f5102a2f
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-24 15:20:57 +0100

    aggiunti commenti

--- DADI/main.js
+++ DADI/main.js
@@ -1,12 +1,15 @@
 alert("Benvenuto nel gioco dei dadi, sfiderai il computer se otterrai il punteggio più alto avrai vinto");
-alert("è il tuo turno, lancia il dado"); 
+//assegno numero all'utente
 let utente = Math.floor( ( Math.random() * 6 ) + 1 );
 console.log(utente);
-alert("il dado segna il numero"); 
+alert("il tuo dado segna il numero"); 
 alert(utente);
+//assegno numero al pc
 let pc = Math.floor( ( Math.random() * 6 ) + 1 );
 alert("il dado del computer segna il numero:");
 alert(pc);
+
+//controllo dei numeri 
 if(pc>utente){
     alert("Mi dispiace, hai perso!")
 } else if (utente>pc){
